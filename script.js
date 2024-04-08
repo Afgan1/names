e314c2d86bcc3a575c306471596c2152da18015e
Afgan Prasetyawan <98010655+Afgan1@users.noreply.github.com>
2024-04-08 20:33:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const output = document.getElementById("output")
+const names = ["anugrah", "ainara", "erika", "febi", "nabila", "ray", "surya"]
+const index = 0
 output.textContent = "Hello World"
+
+
+setInterval(()=>{
+  output.textContent = names[index % names.length]
+}, 750)
